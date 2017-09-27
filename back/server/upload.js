e49d3656d8d1691463ee3dbb6e40bb7304ad78a3
heguofeng <32927202@qq.com>
2017-09-27 14:15:24 +0800

    修改图片路径

--- back/server/upload.js
+++ back/server/upload.js
@@ -3,6 +3,7 @@ const path = require('path')
 const os = require('os')
 const fs = require('fs')
 const Busboy = require('busboy')
+const config = require('../config')
 
 /**
  * 同步创建文件目录
@@ -66,7 +67,7 @@ function uploadFile(ctx, options) {
                 result.success = true
                 result.message = '文件上传成功'
                 result.data = {
-                    pictureUrl: `//${ctx.host}/static/uploads/${fileType}/${fileName}`
+                    pictureUrl: `//${config.host}:${config.port}/static/uploads/${fileType}/${fileName}`
                 }
                 console.log('文件上传成功！')
                 resolve(result)
